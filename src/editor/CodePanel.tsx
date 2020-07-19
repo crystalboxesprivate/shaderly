0c68620ee2d4f1d2514c398538e99f3d2d4e8294
crystalboxes <ursus95@gmail.com>
2020-07-19 16:38:12 +0300

    update code panel

--- src/editor/CodePanel.tsx
+++ src/editor/CodePanel.tsx
@@ -1,41 +1,8 @@
 import { WebView, WebViewMessageEvent } from 'react-native-webview'
-import React, { useState, useRef, useEffect } from 'react';
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
-import { useEditorSource } from './source-loading';
-import { overlayLog } from '../utils/DebugOverlay';
-
-const shaderCode = `#version 150
-uniform float time;
-uniform vec2 resolution;
-uniform vec2 mouse;
-uniform vec3 spectrum;
-
-uniform sampler2D texture0;
-uniform sampler2D texture1;
-uniform sampler2D texture2;
-uniform sampler2D texture3;
-uniform sampler2D prevFrame;
-uniform sampler2D prevPass;
-
-in VertexData
-{
-    vec4 v_position;
-    vec3 v_normal;
-    vec2 v_texcoord;
-} inData;
-
-out vec4 fragColor;
-
-void main(void)
-{
-    vec2 uv = -1. + 2. * inData.v_texcoord;
-    fragColor = vec4(
-        abs(sin(cos(time+3.*uv.y)*2.*uv.x+time)),
-        abs(cos(sin(time+2.*uv.x)*3.*uv.y+time)),
-        spectrum.x * 100.,
-        1.0);
-        
-}`
+import React, { useState, useRef, useEffect } from 'react'
+import { View, StyleSheet, Text, Dimensions } from 'react-native'
+import { useEditorSource } from './source-loading'
+import { overlayLog } from '../utils/DebugOverlay'
 
 function ErrorDispay({ messages }: { messages?: string[] }) {
   return !messages ? null : (
@@ -50,12 +17,11 @@ function ErrorDispay({ messages }: { messages?: string[] }) {
 }
 
 type WebViewMessage = {
-  type: string,
+  type: string
   textValue?: string
 }
 
-export function CodePanel({ initialSource }: { initialSource?: string }) {
-  initialSource = !initialSource ? shaderCode : initialSource as string
+export function CodePanel({ initialSource }: { initialSource: string }) {
   const [currentShaderSource, setCurrentShaderSource] = useState(initialSource)
   const source = useEditorSource(initialSource)
   const webView: React.MutableRefObject<WebView | null> = useRef(null)
@@ -73,14 +39,18 @@ export function CodePanel({ initialSource }: { initialSource?: string }) {
     overlayLog('pending one shader compilation...')
   }, [currentShaderSource])
 
-  return <View style={styles.codePanel}>
-    <WebView style={styles.webView} ref={webView}
-      onMessage={handleMessage}
-      originWhitelist={['*']}
-      source={{ html: source }
-      } />
-    <ErrorDispay messages={['diso', 'dsds']} />
-  </View>
+  return (
+    <View style={styles.codePanel}>
+      <WebView
+        style={styles.webView}
+        ref={webView}
+        onMessage={handleMessage}
+        originWhitelist={['*']}
+        source={{ html: source }}
+      />
+      <ErrorDispay />
+    </View>
+  )
 }
 const styles = StyleSheet.create({
   container: {
@@ -90,13 +60,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   codePanel: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
     backgroundColor: 'rgba(255,255,255,0.0)',
-    flex: 1
+    flex: 1,
   },
   webView: {
     backgroundColor: 'rgba(255,255,255,0.0)',
@@ -109,7 +79,6 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 10,
     borderBottomColor: 'white',
-    borderBottomWidth: 1
-  }
-
-});
+    borderBottomWidth: 1,
+  },
+})
